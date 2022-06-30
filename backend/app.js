e4efc1a8d7945f580d982a382820a5f13765ed2e
Jerome Boivin <jeromeboivin@hotmail.com>
2022-06-30 16:13:19 +0200

    ajout de l'authentification

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 
 app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
@@ -13,5 +14,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+  mongoose.connect('mongodb+srv://Jeje82:Maya250115@cluster0.znxiptp.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
